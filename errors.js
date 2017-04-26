2d2bbb28b0e13b91d20acb24e9ba8434f41942f2
SIGNN OUT OF YOUR GITHUB <7plutkovsky@stpatrick.org>
2017-04-26 14:01:17 -0500

    Add files via upload

---
+++ errors.js
@@ -0,0 +1,22 @@
+//type checkString below
+function checkString(str){
+  if(str.length >= 8 && str.length <= 20){
+    return str;
+  }
+  else{
+    throw RangeError("Not valid length");
+  }
+}
+
+
+//type getString below
+
+function getString(str){
+  try{
+    str = checkString(str);
+  }
+  catch(e){
+    console.log(e.message);
+    setNumber();
+  }
+}
